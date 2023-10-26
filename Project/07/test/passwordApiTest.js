6a75a000c7c6b56fc9ad1a9abaab4372285a963c
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2023-10-26 15:41:29 -0400

    Update passwordApiTest.js

--- Project/07/test/passwordApiTest.js
+++ Project/07/test/passwordApiTest.js
@@ -4,19 +4,19 @@ function includes(values, item) {
   return values.indexOf(item) >= 0;
 }
 
-describe("CS142 Photo App: Password API Tests", function () {
-  let cs142password;
+describe("Photo App: Password API Tests", function () {
+  let password;
 
   describe("test makePasswordEntry", function (done) {
     it("can get the function from the module by require", function (done) {
-      cs142password = require("../cs142password");
-      assert(cs142password);
-      assert.strictEqual(typeof cs142password.makePasswordEntry, "function");
+      password = require("../password");
+      assert(password);
+      assert.strictEqual(typeof password.makePasswordEntry, "function");
       done();
     });
 
     it("can make a password with a hash and salt", function (done) {
-      const pwd = cs142password.makePasswordEntry("TestPassword");
+      const pwd = password.makePasswordEntry("TestPassword");
       assert.strictEqual(typeof pwd, "object");
       assert.strictEqual(typeof pwd.hash, "string");
       assert.strictEqual(pwd.hash.length, 40);
@@ -29,7 +29,7 @@ describe("CS142 Photo App: Password API Tests", function () {
       const saltsSeen = [];
       const hashsSeen = [];
       for (let i = 0; i < 100; i++) {
-        const pwd = cs142password.makePasswordEntry("TestPassword");
+        const pwd = password.makePasswordEntry("TestPassword");
         assert(!includes(saltsSeen, pwd.salt), "duplicate salt returned");
         assert(!includes(hashsSeen, pwd.hash), "duplicate hash returned");
         saltsSeen.push(pwd.salt);
@@ -40,22 +40,22 @@ describe("CS142 Photo App: Password API Tests", function () {
   });
 
   describe("test doesPasswordMatch", function (done) {
-    let cs142password;
+    let password;
 
     it("can get the function from the module by require", function (done) {
-      cs142password = require("../cs142password");
-      assert(cs142password);
-      assert.strictEqual(typeof cs142password.doesPasswordMatch, "function");
+      password = require("../password");
+      assert(password);
+      assert.strictEqual(typeof password.doesPasswordMatch, "function");
       done();
     });
 
     it("can validate a password returned by makePasswordEntry", function () {
-      const pwd = cs142password.makePasswordEntry("TestPassword");
+      const pwd = password.makePasswordEntry("TestPassword");
       assert(
-        cs142password.doesPasswordMatch(pwd.hash, pwd.salt, "TestPassword")
+        password.doesPasswordMatch(pwd.hash, pwd.salt, "TestPassword")
       );
       assert(
-        !cs142password.doesPasswordMatch(pwd.hash, pwd.salt, "NotTestPassword")
+        !password.doesPasswordMatch(pwd.hash, pwd.salt, "NotTestPassword")
       );
     });
   });
